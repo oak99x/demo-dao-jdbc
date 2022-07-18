10675b9bfbbbfdf8a7830f7741e4ba362baa1792
Mateus <mateus.oak99@gmail.com>
2022-07-18 12:24:40 -0300

    DAO's

---
+++ src/dao/DepartmentDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+
+import entities.Department;
+
+public interface DepartmentDAO {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
